3a12231368a1e015789acf798c593701268724dd
BBerg <48287180+BBerg-GitHub@users.noreply.github.com>
2022-02-22 03:58:20 -0800

    Add files via upload

---
+++ Progress Bar/script.js
@@ -0,0 +1,60 @@
+/* import items to use */
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+/* select multiple items same class name */
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+    /* test if it can go above the length of circles */
+    console.log(currentActive)
+
+    update() /* update the DOM */
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+    /* test if it can go above the length of circles */
+    console.log(currentActive)
+
+    update() /* update the DOM */
+})
+
+/* update the dom */
+function update() {
+    circles.forEach((circle, index) => {
+        if(index < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    /* get active circles */
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length-1) / (circles.length-1) * 100 + '%'
+
+    if(currentActive === 1){
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+
+}
+
+
